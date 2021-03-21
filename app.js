1808e497b93599e6ed3280af41bec4028b89a591
youssefmimouni08 <youssef.mimouni@esprit.tn>
2021-03-21 17:45:20 +0100

    user related routes

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 
 // Connect Database
 connectDB();
+// init midlleware
+app.use(express.json({ extended: false }));
+app.get('/', (req, res) => res.send('API Running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const port = process.env.PORT || 5000;
 
