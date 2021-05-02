4105bdb49ae8c5a749f2495827084a03533be95f
youssefmimouni08 <youssef.mimouni@esprit.tn>
2021-05-02 21:46:25 +0100

    a

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var corsOptions = {
   origin: '*',
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 app.use(cors(corsOptions));
 // init midlleware
