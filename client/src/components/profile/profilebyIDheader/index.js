fc4ab9755754bea4d51c8c49e59165edb457b104
youssefmimouni08 <youssef.mimouni@esprit.tn>
2021-05-02 22:42:19 +0100

    aa

--- client/src/components/profile/profilebyIDheader/index.js
+++ client/src/components/profile/profilebyIDheader/index.js
@@ -9,7 +9,7 @@ const profilebyIDheader = ({
   followUser,
   profile: { profile, loader },
 
-  social: { instagram, twitter, facebook, youtube, linkedin }
+  social
 }) => {
   const user = profile.user;
   return (
@@ -33,28 +33,28 @@ const profilebyIDheader = ({
               </Button>
               <h3>Socials :</h3>
               <ul className=" gx-social-link ">
-                {instagram ? (
+                {social && social.instagram ? (
                   <li>
                     <Icon type="instagram" />
                   </li>
                 ) : null}
-                {linkedin ? (
+                {social && social.linkedin ? (
                   <li>
                     <Icon type="linkedin" />
                   </li>
                 ) : null}
 
-                {twitter ? (
+                {social && social.twitter ? (
                   <li>
                     <Icon type="twitter" />
                   </li>
                 ) : null}
-                {facebook ? (
+                {social && social.facebook ? (
                   <li>
                     <Icon type="facebook" />
                   </li>
                 ) : null}
-                {youtube ? (
+                {social && social.youtube ? (
                   <li>
                     <Icon type="youtube" />
                   </li>
