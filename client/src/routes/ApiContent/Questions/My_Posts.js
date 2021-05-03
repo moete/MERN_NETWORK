3de4b91b03d31ab354491a531c19552c9029a252
mariem bechikh ali <mariem.bechikhali@esprit.tn>
2021-05-03 21:40:53 +0100

    console errors & get only my posts

--- client/src/routes/ApiContent/Questions/My_Posts.js
+++ client/src/routes/ApiContent/Questions/My_Posts.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Drawer, Col } from "antd";
+import { Button, Row, Col } from "antd";
 import CustomScrollbars from "util/CustomScrollbars";
 import axios from "axios";
 
@@ -8,10 +8,9 @@ import AppModuleHeader from "components/AppModuleHeader/index";
 import IntlMessages from "util/IntlMessages";
 import _ from "lodash";
 import { Link } from "react-router-dom";
-import CircularProgress from "components/CircularProgress/index";
-
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../../appRedux/actions/profile";
+import CircularProgress from "components/CircularProgress/index";
 
 const Question = props => (
   <div className="gx-contact-item">
@@ -72,169 +71,6 @@ const Question = props => (
     </div>
   </div>
 );
-const User = ({ profile }) => (
-  <div>
-    <div className="gx-wall-scroll">
-      <div>
-        <div className="gx-profileon">
-          <div className="gx-profileon-thumb gx-profileon-thumb-htctrcrop">
-            <img src={profile.user.avatar} alt=""></img>
-          </div>
-          <div
-            className="gx-profileon-content"
-            style={{ backgroundColor: "#38424b", opacity: "85%" }}
-          >
-            <p className="gx-profileon-title">{profile.user.name} </p>
-            <span className="gx-fs-sm">{profile.location}</span>
-          </div>
-        </div>
-        <div className="gx-follower gx-text-center">
-          <ul className="gx-follower-list">
-            <li>
-              <span className="gx-follower-title">2k+</span>
-              <span>Followers</span>
-            </li>
-            <li>
-              <span className="gx-follower-title">847</span>
-              <span>Following</span>
-            </li>
-            <li>
-              <span className="gx-follower-title">327</span>
-              <span>project</span>
-            </li>
-          </ul>
-        </div>
-        <div className="gx-mb-xl-4 gx-mb-3">
-          <p>You are following Chelsea Jones @chelsea</p>
-          <button
-            type="button"
-            className="ant-btn gx-btn-sm gx-mb-0 ant-btn-primary"
-          >
-            <span>Unfollow</span>
-          </button>
-        </div>
-        <div className="gx-entry-sec">
-          <h2 className="gx-entry-title ">
-            Interests
-            <span className="gx-text-primary gx-fs-md gx-pointer gx-ml-auto gx-d-none gx-d-sm-block"></span>
-          </h2>
-          <ul className="gx-list-inline">
-            <li>
-              <span className="gx-link gx-btn gx-btn-white gx-mb-10">
-                javaScript
-              </span>
-            </li>
-            <li>
-              <span className="gx-link gx-btn gx-btn-white gx-mb-10">
-                ReactJs
-              </span>
-            </li>
-            <li>
-              <span className="gx-link gx-btn gx-btn-white gx-mb-10">
-                Symfony
-              </span>
-            </li>
-            <li>
-              <span className="gx-link gx-btn gx-btn-white gx-mb-10">php</span>
-            </li>
-            <li>
-              <span className="gx-link gx-btn gx-btn-white gx-mb-10">
-                Mongoose
-              </span>
-            </li>
-          </ul>
-        </div>
-        <div className="gx-entry-sec">
-          <h2 className="gx-entry-title ">
-            <span>
-              Friends - 530
-              <span className="gx-text-grey">(27 Mutual)</span>
-            </span>
-            <span className="gx-text-primary gx-fs-md gx-pointer gx-ml-auto gx-d-none gx-d-sm-block"></span>
-          </h2>
-          <ul className="gx-fnd-list">
-            <li className="gx-mb-2">
-              <div className="gx-user-fnd">
-                <img alt="..." src="https://via.placeholder.com/150x150"></img>
-                <div className="gx-user-fnd-content">
-                  <span className="ant-badge ant-badge-status ant-badge-not-a-wrapper">
-                    <span className="ant-badge-status-dot ant-badge-status-success"></span>
-                    <span className="ant-badge-status-text"></span>
-                  </span>
-                  <h6>Chelsea</h6>
-                </div>
-              </div>
-            </li>
-            <li className="gx-mb-2">
-              <div className="gx-user-fnd">
-                <img alt="..." src="https://via.placeholder.com/150x150"></img>
-                <div className="gx-user-fnd-content">
-                  <span className="ant-badge ant-badge-status ant-badge-not-a-wrapper">
-                    <span className="ant-badge-status-dot ant-badge-status-error"></span>
-                    <span className="ant-badge-status-text"></span>
-                  </span>
-                  <h6>Kenery Thomson</h6>
-                </div>
-              </div>
-            </li>
-            <li className="gx-mb-2">
-              <div className="gx-user-fnd">
-                <img alt="..." src="https://via.placeholder.com/150x150"></img>
-                <div className="gx-user-fnd-content">
-                  <span className="ant-badge ant-badge-status ant-badge-not-a-wrapper">
-                    <span className="ant-badge-status-dot ant-badge-status-warning"></span>
-                    <span className="ant-badge-status-text"></span>
-                  </span>
-                  <h6>Amanda</h6>
-                </div>
-              </div>
-            </li>
-            <li className="gx-mb-2">
-              <div className="gx-user-fnd">
-                <img alt="..." src="https://via.placeholder.com/150x150"></img>
-                <div className="gx-user-fnd-content">
-                  <span className="ant-badge ant-badge-status ant-badge-not-a-wrapper">
-                    <span className="ant-badge-status-dot ant-badge-status-warning"></span>
-                    <span className="ant-badge-status-text"></span>
-                  </span>
-                  <h6>Joshua</h6>
-                </div>
-              </div>
-            </li>
-            <li className="gx-mb-2">
-              <div className="gx-user-fnd">
-                <img alt="..." src="https://via.placeholder.com/150x150"></img>
-                <div className="gx-user-fnd-content">
-                  <span className="ant-badge ant-badge-status ant-badge-not-a-wrapper">
-                    <span className="ant-badge-status-dot ant-badge-status-warning"></span>
-                    <span className="ant-badge-status-text"></span>
-                  </span>
-                  <h6>Alex Mulski</h6>
-                </div>
-              </div>
-            </li>
-            <li className="gx-mb-2">
-              <div className="gx-user-fnd">
-                <img alt="..." src="https://via.placeholder.com/150x150"></img>
-                <div className="gx-user-fnd-content">
-                  <span className="ant-badge ant-badge-status ant-badge-not-a-wrapper">
-                    <span className="ant-badge-status-dot ant-badge-status-warning"></span>
-                    <span className="ant-badge-status-text"></span>
-                  </span>
-                  <h6>Stella Johnson</h6>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="track-horizontal">
-        <div></div>
-      </div>
-      <div></div>
-    </div>
-  </div>
-);
 const filterOptions = [
   {
     id: 1,
@@ -254,18 +90,12 @@ class MyPosts extends Component {
 
     this.state = {
       noContentFoundMessage: "No Posts found",
-      alertMessage: "",
-      showMessage: false,
-      selectedSectionId: 1,
-      drawerState: false,
       searchUser: "",
       filterOption: "My posts",
-      allContact: [],
-      contactList: [],
-      selectedContact: null,
-      addContactState: false,
       questions: [],
-      isLoading: true
+      isLoading: true,
+      owner:[]
+
     };
   }
 
@@ -293,12 +123,7 @@ class MyPosts extends Component {
       questions: this.state.questions.filter(el => el._id !== id)
     })
   }
-  questionList() {
-    return this.state.questions.map(currentquestion => {
-      return <Question questions={currentquestion} deleteQuestion={this.deleteQuestion} key={currentquestion._id}/>;
-    });
-  }
-  User() {
+  questionList ( profile ){
     if (this.state.isLoading) {
       return (
         <div className="gx-loader-view">
@@ -306,10 +131,19 @@ class MyPosts extends Component {
         </div>
       );
     } else {
-      return <User profile={this.props.profile.profile} />;
+      return this.state.questions.map(currentquestion => {
+        if (currentquestion.owner.user === this.props.profile.profile.user._id)
+        {
+          this.props.getCurrentProfile();
+          return <Question questions={currentquestion} deleteQuestion={this.deleteQuestion} key={currentquestion._id}/>;
+
+        }
+      });
     }
+    
   }
-  ContactSideBar = user => {
+
+  SideBar = user => {
     return (
       <div className="gx-module-side">
         <div className="gx-module-side-header">
@@ -385,42 +219,37 @@ class MyPosts extends Component {
         break;
     }
   };
- 
+  onSearch(event) {
+    this.setState({
+      search: event.target.value.substr(0, 100)
+    });
+    this.props.getCurrentProfile();
+    if (this.state.isLoading) {
+      setTimeout(() => {
+        this.setState({ isLoading: false });
+      }, 5000);
+    }
+  }
   render() {
-    const { questions, user, drawerState, noContentFoundMessage } = this.state;
+    const { questions, user, noContentFoundMessage } = this.state;
     return (
       <div className="gx-main-content">
         <div className="gx-app-module">
-          <div className="gx-d-block gx-d-lg-none">
-            <Drawer
-              placement="left"
-              closable={false}
-              visible={drawerState}
-            /*  onClose={this.onToggleDrawer.bind(this)}*/
-            ></Drawer>
-            
-          </div>
           <div className="gx-module-sidenav gx-d-none gx-d-lg-flex">
-            {this.ContactSideBar(user)}
+            
+            {this.SideBar(user)}
           </div>
-
+         
           <div className="gx-module-box">
             <div className="gx-module-box-header">
-              <span className="gx-drawer-btn gx-d-flex gx-d-lg-none">
-                <i
-                  className="icon icon-menu gx-icon-btn"
-                  aria-label="Menu"
-              /*    onClick={this.onToggleDrawer.bind(this)}*/
-                />
-              </span>
 
               <AppModuleHeader
                 placeholder="Search posts"
                 notification={false}
                 apps={false}
                 user={this.state.user}
-                /*onChange={this.updateContactUser.bind(this)}*/
-                value={this.state.searchUser}
+                onChange={this.onSearch.bind(this)}
+               value={this.state.searchUser}
               />
             </div>
             <div className="gx-module-box-content">
@@ -435,9 +264,7 @@ class MyPosts extends Component {
                   this.questionList()
                 )}
               </CustomScrollbars>
-              <Col className="gutter-row" span={6}>
-            {this.User()}
-          </Col>
+             
             </div>
           </div>
         </div>
@@ -449,4 +276,4 @@ const mapStateToProps = state => ({
   auth: state.auth,
   profile: state.profile
 });
-export default connect(mapStateToProps, { getCurrentProfile }) (MyPosts);
+export default connect(mapStateToProps, { getCurrentProfile })(MyPosts);
