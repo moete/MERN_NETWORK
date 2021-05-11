b85c170e5d2bba8ae24f6d563741d15887677b2d
youssefmimouni08 <youssef.mimouni@esprit.tn>
2021-05-11 03:03:10 +0100

    jobcontroller

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const {
-    getAllJobs,
-    getJobById,
-    createJob,
-    updateJobById,
-    deleteJob,
-    getJobStat
-} = require("../controllers/Job.controller");
+  getAllJobs,
+  getJobById,
+  createJob,
+  updateJobById,
+  deleteJob,
+  getJobStat,
+} = require('../controllers/job.controller');
 
 const router = new express.Router();
 
-router.post("/create", createJob);
-router.get("/showJob", getAllJobs);
-router.get("/stat", getJobStat);
-router.get("/showbyId", getJobById);
-router.patch("/updateJob/:id", updateJobById);
-router.delete("/deleteJob/:id", deleteJob);
+router.post('/create', createJob);
+router.get('/showJob', getAllJobs);
+router.get('/stat', getJobStat);
+router.get('/showbyId', getJobById);
+router.patch('/updateJob/:id', updateJobById);
+router.delete('/deleteJob/:id', deleteJob);
 
 module.exports = router;
